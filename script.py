2b9fe327cdc0233c6fc2054b6425df2df981f8d2
Ice424 <e4elliotd@gmail.com>
2023-10-16 17:54:06 +0100

    Values can be extracted

--- script.py
+++ script.py
@@ -1,7 +1,28 @@
 import json
+
+Types = []
+Origin = []
+From = []
+To = []
+
 with open("model.bbmodel", "r") as model:
-  data = json.load(model)
-print(data)
-newData = json.dumps(data, indent=4)
+  model = json.load(model)
+
+elements = model["elements"]
+for data in elements:
+  Types.append(data["name"])
+  Origin.append(data["origin"])
+  From.append(data["from"])
+  To.append(data["to"])
+
+print (Types)
+print (Origin)
+print (From)
+print (To)
+
+
+
+
+newData = json.dumps(elements, indent=4)
 with open("new.json", "w") as file:
   file.write(newData)
