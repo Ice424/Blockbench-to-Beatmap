46f948b6fc66c44db1e6c9cd0c933e7576a475f7
Ice424 <110476854+Ice424@users.noreply.github.com>
2023-10-16 12:17:36 +0000

    anothercommit

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
 import json
 with open("model.bbmodel", "r") as model:
-    data = json.load(model)
+  data = json.load(model)
 print(data)
-newData = ""
 newData = json.dumps(data, indent=4)
 with open("new.json", "w") as file:
   file.write(newData)
