f0ff7dc44d9d51653fa0067e179c4333aadc6253
Ice424 <110476854+Ice424@users.noreply.github.com>
2023-10-16 12:11:35 +0000

    Inital Commit

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("go away")
+import json
+with open("model.bbmodel", "r") as model:
+    data = json.load(model)
+print(data)
+newData = ""
+newData = json.dumps(data, indent=4)
+with open("new.json", "w") as file:
+  file.write(newData)
