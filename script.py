0f80cb60211142bad91c50e7bbd82820dca7594d
Ice424 <110476854+Ice424@users.noreply.github.com>
2023-10-20 15:54:53 +0000

    commit

--- script.py
+++ script.py
@@ -1,65 +1,81 @@
 import json
 
-Types = []
-Position = []
-Corners1 = []
-Corners2 = []
-Scale = []
+types = []
+position = []
+corners1 = []
+corners2 = []
+scale = []
+uuid = []
+tracks = []
+children = []
+skip = 0
 
 # this is chat gpt magic
-def calculate_cube_scale(Position, corner1, corner2):
+
+
+def calculate_cube_scale(position, corner1, corner2):
     # Calculate scale values along x, y, and z axes
     scale_x = abs(corner2[0] - corner1[0])
     scale_y = abs(corner2[1] - corner1[1])
     scale_z = abs(corner2[2] - corner1[2])
 
     return scale_x, scale_y, scale_z
-def calculate_multiple_cubes_scales(Position, Corners1, Corners2):
+
+
+def calculate_multiple_cubes_scales(position, corners1, corners2):
 
     # Calculate scale values for each cube
     scales = []
-    for i in range(len(Position)):
-        scale = calculate_cube_scale(Position[i], Corners1[i], Corners2[i])
+    for i in range(len(position)):
+        scale = calculate_cube_scale(position[i], corners1[i], corners2[i])
         scales.append(scale)
 
     return scales
+# end of chatgpt  magic
+
 
 # Open file
 with open("model.bbmodel", "r") as model:
-  model = json.load(model)
+    model = json.load(model)
 
-# Find and store data
+# Find and store cubes
 elements = model["elements"]
-for data in elements:
-  Types.append(data["name"])
-  Position.append(data["origin"])
-  Corners1.append(data["from"])
-  Corners2.append(data["to"])
+for i in elements:
+    types.append(i["name"])
+    position.append(i["origin"])
+    corners1.append(i["from"])
+    corners2.append(i["to"])
+    uuid.append(i["uuid"])
 
+# find and store tracks
+outliner = model["outliner"]
+for y in outliner:
+    children.append(y["children"])
 
+print(outliner)
+print(elements)
+
+print(children)
+print(uuid)
 # Calculate scale values for multiple cubes
-Scale = calculate_multiple_cubes_scales(Position, Corners1, Corners2)
-
-print(Types)
-print(Scale)
-print(Position)
+scale = calculate_multiple_cubes_scales(position, corners1, corners2)
 
 Enviroment = []
 
 # create the json file
-for x in range(len(Scale)):
-  Enviroment.append(
-      {
-          "geometry": {
-          "type": Types[x],
-          "material": "standard"
-          },
-        "scale": Scale[x],
-        "position": Position[x]
+for x in range(len(scale)):
+    Enviroment.append(
+        {
+            "geometry": {
+                "type": types[x],
+                "material": "standard"
+            },
+            "scale": scale[x],
+            "position": position[x]
         }
-      )
+    )
 
-# dump 
+# dump
 newData = json.dumps(Enviroment, indent=4)
 with open("new.json", "w") as file:
-  file.write(newData)
+    file.write(newData)
